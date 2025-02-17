8ecb727c88d41c4bd990d73550cef2f302c7fb59
JHONCE79 <jhonsteven.163@gmail.com>
2025-02-17 17:48:53 -0500

    new change

--- Ejercicio6.py
+++ Ejercicio6.py
@@ -2,4 +2,17 @@
 Anagramas
 """
 
-def anagrama()
+def anagrama(text):
+    if text == "":
+        return []
+    else:
+        primero = text[0]
+        restante = text[1:]
+        combinations = []
+        for i in anagrama(restante):
+            nuevo = text[:i] + primero + text[i:]
+            combinations.append(nuevo)
+        return combinations
+                   
+
+print(anagrama("abc"))
